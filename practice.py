493a4316e0f344624436343053e95cec1947fdf0
Rakin Bhuiyan <84994510+Rakin106@users.noreply.github.com>
2025-09-04 13:11:37 +0200

    Clean up practice.py by removing comments

--- practice.py
+++ practice.py
@@ -1,14 +1,3 @@
-# practice.py - simple code examples
-
-# TODO: practice more Python functions
-print('Learning GitHub commits')
-
-
-
-    print('Hello, world!')
-# TODO: practice more Python functions
-
-# TODO: practice more Python functions
 # def hello_world():
     return a + b
 print('Learning GitHub commits')

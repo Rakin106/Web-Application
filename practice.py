b7922c40714c6f5bda96e99af48c578e0717b1d7
Rakin Bhuiyan <84994510+Rakin106@users.noreply.github.com>
2025-09-06 01:50:39 +0200

    oops forgot this... in practice.py

--- practice.py
+++ practice.py
@@ -1,23 +1,23 @@
 # def hello_world():
     return a + b
-print('Learning GitHub commits')
-print('Learning GitHub commits')
-
-  # updated
-# def add(a, b):
-
-
-try:
-    print(10/2)
 except Exception as e:
-    print('error', e)
+
 def subtract(a, b):
-    return a - b
-try:
+    return a / b if b else None
+print('Learning GitHub commits')
+from math import sqrt
+    print(10/2)
+
     print(10/2)
 except Exception as e:
     print('error', e)
 def divide(a, b):
-    return a / b if b else None
-from math import sqrt
+print('Learning GitHub commits')
+# def add(a, b):
+    return a - b
 print('sqrt(9)=', sqrt(9))
+try:
+    print('error', e)
+  # updated
+try:
+

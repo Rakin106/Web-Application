d75f10bf31e264f1e82ba15eebef5aa3fd383f78
Rakin Bhuiyan <84994510+Rakin106@users.noreply.github.com>
2025-09-07 02:52:16 +0200

    Update practice.py

--- practice.py
+++ practice.py
@@ -8,16 +8,5 @@ print('Learning GitHub commits')
 from math import sqrt
     print(10/2)
 
-    print(10/2)
-except Exception as e:
-    print('error', e)
-def divide(a, b):
-print('Learning GitHub commits')
-# def add(a, b):
-    return a - b
-print('sqrt(9)=', sqrt(9))
-try:
-    print('error', e)
-  # updated
-try:
+
 
